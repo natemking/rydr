c7a6c3986c06ded9350ef4c9b1f6ca916a3ce124
wylie <wyliedavid1984@gmail.com>
2021-02-12 16:49:16 -0500

    have the routes setup

--- routes/api/dbRoutes.js
+++ routes/api/dbRoutes.js
@@ -1,5 +1,24 @@
 const router = require('express').Router();
 const dbController = require('../../controllers/dbController');
 
-router.route("/:id")
-.get(dbController.findById);
+
+router.route('/band/:id')
+.get(dbController.findByIdBand)
+.put(dbController.findByIdBand)
+.put(dbController.updateBand)
+.delete(dbController.deleteBand);
+
+router.route('/venue/:id')
+.get(dbController.findByIdVenue)
+.post(dbController.createReview)
+.update(dbController.updateReview);
+
+router.route('/venue/:name')
+.get(dbController.findByName)
+.post(dbController.CreateVenue);
+
+router.route('/venue/:rating')
+.get(dbController.findByRating)
+.put(dbController.updateReview);
+
+module.exports = router
