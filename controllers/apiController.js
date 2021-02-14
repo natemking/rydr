4ddd40f977f0547e27dc0b0949968352336cfed4
Dmitri So <dmitriso@yahoo.com>
2021-02-14 13:42:09 -0500

    Changing code in the api controller.

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,20 +1,22 @@
 const axios = require('axios');
-const clientId= 'GSIEWB3V0L4OOFEWHBX4R0K1MOB0CJOJWGLSHHEP0DPKHNP1';
-const clientSecret= 'IABLARJ3OWCNSLW1VR00W4IB33FK3H1MLP32XJF5JWW3LFL4';
-const category = "4bf58dd8d48988d1e5931735";
+// const clientId= 'GSIEWB3V0L4OOFEWHBX4R0K1MOB0CJOJWGLSHHEP0DPKHNP1';
+// const clientSecret= 'IABLARJ3OWCNSLW1VR00W4IB33FK3H1MLP32XJF5JWW3LFL4';
+// const category = "4bf58dd8d48988d1e5931735";
+const query = `&limit=100&near=${city}&radius=${radius}`;
+const url = `https://api.foursquare.com/v2/venues/search?client_id=GSIEWB3V0L4OOFEWHBX4R0K1MOB0CJOJWGLSHHEP0DPKHNP1&client_secret=IABLARJ3OWCNSLW1VR00W4IB33FK3H1MLP32XJF5JWW3LFL4&v=20180323&categoryId=4bf58dd8d48988d1e5931735`;
+// 
 const limit = '';
 const city = '';
 const radius = '';
-// const query = `${url}?client_id=${process.env.clientId}&client_secret=${process.env.clientSecret}&v=20180323&limit=${limit}&categoryId=${category}&near=${city}&radius=${radius}`;
-// const url = `https://api.foursquare.com/v2/venues/search${query}`;
 
 module.exports = {
     async searchVenueApi(req,res) {
         try {
-            const response = await axios.get("https://api.foursquare.com/v2/venues/search?client_id=GSIEWB3V0L4OOFEWHBX4R0K1MOB0CJOJWGLSHHEP0DPKHNP1&client_secret=IABLARJ3OWCNSLW1VR00W4IB33FK3H1MLP32XJF5JWW3LFL4&v=20180323&limit=30&categoryId=4bf58dd8d48988d1e5931735&near=philadelphia");
-            console.log(response.data)
-            res.json(response.data)
+            const response = await axios.get(`${url}&limit=100&near=${city}&radius=${radius}`);
+            console.log(response.response.venues)
+            res.send(response.response.venues)
             
-        } catch(err) {console.error(err)} 
+        } catch(err) { console.error(err) } 
     }
 }
+
