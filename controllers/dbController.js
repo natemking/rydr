e8e1980ca916188940166fde3b53acc0800bb036
wylie <wyliedavid1984@gmail.com>
2021-02-14 11:37:55 -0500

    added populate

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -1,18 +1,18 @@
-const db = require("../models");
+const db = require('../models');
 
 // DEFINING METHODS FOR THE DB CONTROLLER TO REFERENCE IN DB ROUTES
 module.exports = {
     // JUST IN CASE: DON'T THINK WE'LL NEED IT
     findAllBand: function (req, res) {
         db.Band
-            .find(req.query)
+            .find(req.query).populate("postedReviews")
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     // FIND Band BY ID
     findByIdBand: function (req, res) {
         db.Band
-            .findById(req.params.id)
+            .findById(req.params.id).populate("postedReviews")
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
@@ -44,14 +44,14 @@ module.exports = {
     },
     findAllVenue: function (req, res) {
         db.Venue
-            .find(req.query)
+            .find(req.query).populate('venueReviews')
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     // FIND VENUE BY ID
     findByIdVenue: function (req, res) {
         db.Venue
-            .findById(req.params.id)
+            .findById(req.params.id).populate('venueReview')
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
@@ -59,7 +59,7 @@ module.exports = {
     findByVenueName: function (req, res){
         const name = req.params.name
         db.Venue
-        .find({venueName: name}).collation( { locale: 'en', strength: 1 })
+        .find({venueName: name}).collation( { locale: 'en', strength: 1 }).populate('venueReviews')
         .then(dbModel => {
             res.json(dbModel)})
         .catch(err => res.status(422).json(err));
@@ -78,22 +78,55 @@ module.exports = {
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
-    // UPDATE VENUE
-    updateVenue: function (req, res) {
-        db.Venue
-            .findOneAndUpdate({
-                _id: req.params.id
-            }, req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
     // FIND VENUES BY RATING
     // COULD BE CHANGED OUT WITH A SORT METHOD ON THE FRONTEND
     findByRating: function (req, res) {
         db.Venue
             .find({rating: {$elemMatch:{rating: req.params.rating}}
-            }, req.body)
+            }, req.body).populate('venueReviews')
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
+    },
+    removeVenue: function (req, res) {
+    db.Venue
+        .findById({
+            _id: req.params.id
+        })
+        .then(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    createReview: function (req, res){
+        db.Review
+        .create(req.body)
+        .then(dbModel =>res.json(dbModel))
+        .catch(err => res.status(422).json(err))
+    },
+    updateReview: function (req, res){
+        db.Review
+        .findOneAndUpdate({_id : req.params.id}, req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err))
+    },
+    getReviewByBand: function (req, res){
+        db.Review
+        .find({author: req.params.author})
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err))
+    },
+    getReviewByVenue: function(req, res){
+        db.Review
+        .find({venue: req.params.venue})
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err))
+    },
+    removeReview: function (req, res) {
+    db.Review
+        .findById({
+            _id: req.params.id
+        })
+        .then(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
     },
 };
--- routes/api/dbRoutes.js
+++ routes/api/dbRoutes.js
@@ -1,25 +1,27 @@
 const router = require('express').Router();
 const dbController = require('../../controllers/dbController');
+const db = require('../../models');
 
 // create a new band user.
 router.route('/band')
 .post(dbController.createBand)
 .get(dbController.findAllBand);
+
 // all routes to manipulate band data by id
 router.route('/band/:id')
 .get(dbController.findByIdBand)
-// should we use a patch instead of put?? Patch is suppposed to only update what is passed in versus the whole object.
 .put(dbController.updateBand)
 .delete(dbController.removeBand);
+
 // get all venues from db
 router.route('/venue')
 .get(dbController.findAllVenue);
+
 // all routes to manipulate venue data by id
 router.route('/venue/:id')
 .get(dbController.findByIdVenue)
 .post(dbController.createVenue)
-// should we use a patch instead of put??
-.put(dbController.updateVenue);
+.delete(dbController.removeVenue)
 
 // create or find venue by name
 router.route('/venue/name/:name')
@@ -29,5 +31,18 @@ router.route('/venue/name/:name')
 // find venue by ratings
 router.route('/venue/rating/:rating')
 .get(dbController.findByRating);
+
+router.route('/reviews')
+.post(dbController.createReview);
+
+router.route('/reviews/:id')
+.put(dbController.updateReview)
+.delete(dbController.removeReview);
+
+router.route('/reviews/band/:band')
+.get(dbController.getReviewByBand);
+
+router.route('/reviews/venue/:venue')
+.get(dbController.getReviewByVenue)
 
 module.exports = router
