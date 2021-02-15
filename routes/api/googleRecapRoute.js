cbca18b9243b1ca0e2f1e8e7af97c1d4bb816d85
Nate M King <72567521+natemking@users.noreply.github.com>
2021-02-15 15:21:41 -0500

    Removed an uneeded requirement in googleRecapRoute.js

--- routes/api/googleRecapRoute.js
+++ routes/api/googleRecapRoute.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiController = require('../../controllers/apiController');
-const db = require('../../models');
+
 
 
 router.route('/:userToken')
