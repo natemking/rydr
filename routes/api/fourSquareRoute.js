e964c0ea5131725a414579e5d41dee204eb3bc04
John Hauth <jkhauth@gmail.com>
2021-02-14 21:23:57 -0500

    search functionality almost complete

--- routes/api/fourSquareRoute.js
+++ routes/api/fourSquareRoute.js
@@ -4,14 +4,10 @@ const apiController = require('../../controllers/apiController');
 
 // ROUTES FOR FOURSQUARE DB
 router.route("/search/:limit/:city/:radius")
-.post(apiController.postVenueApi)
+// .post(apiController.postVenueApi)
 .get(apiController.getVenueApi);
 
 module.exports = router
-
-
-
-
 
 
 // // https://api.foursquare.com/v2/venues/search?client_id=GSIEWB3V0L4OOFEWHBX4R0K1MOB0CJOJWGLSHHEP0DPKHNP1&client_secret=IABLARJ3OWCNSLW1VR00W4IB33FK3H1MLP32XJF5JWW3LFL4&v=20180323&limit=30&categoryId=4bf58dd8d48988d1e5931735&near=philadelphia
