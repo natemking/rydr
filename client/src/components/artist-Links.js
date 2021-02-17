4722839e3abf7ab6300a71e3107d6eba2177c4b5
Dmitri So <dmitriso@yahoo.com>
2021-02-17 12:04:52 -0500

    Fixing syntax errors with bandLinks.

--- client/src/components/artist-Links.js
+++ client/src/components/artist-Links.js
@@ -1,13 +1,8 @@
 import React from 'react';
 
 const ArtistLinks = ({artist, isLoading}) => {
-<<<<<<< Updated upstream
-    if (artist.bandLinks === undefined){
-        artist.bandLinks = ["","","","",""]
-=======
     if (!artist.bandLinks){
-        artist.bandLinks = ["","",""]
->>>>>>> Stashed changes
+        artist.bandLinks = ["","","","",""]
     }
     return isLoading ? (<h1>Loading....</h1>) : 
     (
@@ -15,8 +10,8 @@ const ArtistLinks = ({artist, isLoading}) => {
             <a href={artist.bandLinks[0].siteUrl}>{artist.bandLinks[0].siteName}</a>
             <a href={artist.bandLinks[1].siteUrl}>{artist.bandLinks[1].siteName}</a>
             <a href={artist.bandLinks[2].siteUrl}>{artist.bandLinks[2].siteName}</a>
-            <a href={artist.bandLinks[3].siteUrl}>{artist.bandLinks[2].siteName}</a>
-            <a href={artist.bandLinks[4].siteUrl}>{artist.bandLinks[2].siteName}</a>
+            <a href={artist.bandLinks[3].siteUrl}>{artist.bandLinks[3].siteName}</a>
+            <a href={artist.bandLinks[4].siteUrl}>{artist.bandLinks[4].siteName}</a>
 
         </div>
  
