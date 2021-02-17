12cfe66e95317a7bfea9e28b99eed83db01643f4
Nate M King <72567521+natemking@users.noreply.github.com>
2021-02-17 11:52:45 -0500

    artistbody component loading error fixes

--- client/src/components/main-artist.js
+++ client/src/components/main-artist.js
@@ -3,40 +3,46 @@ import ReviewsTable from "./artist-ReviewsTable";
 import Artist from "./artist-artistBody";
 import API from "../utils/API";
 
-const BandPage = ( {match} ) => {
-  const [artist, setArtist] = useState ([])
-  const [isLoading, setisLoading] = useState(true)
+const BandPage = ({ match }) => {
+  const [artist, setArtist] = useState ('')
+  const [isLoading, setIsLoading] = useState(true)
   const [reviews, setReviews] = useState([])
 
 useEffect(() => {
-  const fetchArtist = async () => {
-    const result = await API.getUser(match.params.id)
-    const userArtist = result.data
-      setArtist(userArtist)
-      setisLoading(false)
-  }
-  fetchArtist()
+  fetchArtistAndReviews();
+  
 }, []);
 
-useEffect(() => {
-  const fetchReviews = async () => {
-    const reviews = await API.getReviewByBand(match.params.id)
-    setReviews(reviews.data)
-    setisLoading(false)
+// useEffect(() => {
+//   fetchReviews();
+// }, []);
+
+
+  const fetchArtistAndReviews = async () => {
+    const artist = await API.getUser(match.params.id);
+    const reviews = await API.getReviewByBand(match.params.id);
+    setArtist(artist.data);
+    setReviews(reviews.data);
+    setIsLoading(false);
   }
-  fetchReviews();
-}, []);
+
+  // const fetchReviews = async () => {
+  //   const reviews = await API.getReviewByBand(match.params.id)
+  //   setReviews(reviews.data)
+  //   setIsLoading(false)
+  // }
 
     return (
-    <div className="d-flex flex-column mt-2 p-2 align-items-center">
-    <Artist artist={artist} isloading={isLoading}/>
-    <div className="d-flex flex-column flex-wrap">
-    <h1><u>Artist Reviews</u></h1>
-    <ReviewsTable reviews={reviews} isloading={isLoading}/>
-    </div>
-    <hr></hr>
-    </div>
-     )
+      
+      <div className="d-flex flex-column mt-2 p-2 align-items-center">
+        <Artist artist={ artist } isLoading={ isLoading } />
+        <div className="d-flex flex-column flex-wrap">
+          <h1><u>Artist Reviews</u></h1>
+          <ReviewsTable reviews={ reviews } isLoading={ isLoading }/>
+        </div>
+        <hr />
+      </div>
+    )
 }
     
 export default BandPage;
