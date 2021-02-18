a37821ef9e4b08142ad77b81abce766bdb4e878b
Nate M King <72567521+natemking@users.noreply.github.com>
2021-02-18 15:47:50 -0500

    When user deletes band link it now dosappears from the DOM

--- client/src/components/artist-Links.js
+++ client/src/components/artist-Links.js
@@ -3,29 +3,29 @@ import API from '../utils/API'
 
 
 const ArtistLinks = ({ artist, isLoading, title }) => {
-
+    // Variable for the different types of links to display
     let links;
     
+    // On click remove user link from DB and DOM
     const handleOnClick = (e) => {
-        console.log(artist._id, e.target.id)
-      API.deleteLink({bandId:artist._id, id:e.target.id});
+        API.deleteLink({bandId:artist._id, id:e.target.id});
+        document.getElementById(e.target.id).parentNode.parentNode.remove();
     }
 
-    // Iterate over band links for display
+    // Iterate over band links for display. Type of the list element is different depending on the page it is being rendered in
     if (title) {
+        // Links for Update Artist screen
         links = artist.bandLinks.map((link) => (
-            <>
-                <li key={ link._id } className='artistLink__li'>
-                    { link.siteName }
-                    <span>
-                        <i className="fa fa-times" aria-hidden="true" id={ link._id } onClick={ handleOnClick }></i>
-                    </span>
-                    <br />
-                </li>
-            </>
-            
+            <li key={ link._id } className='artistLink__li'>
+                { link.siteName }
+                <span>
+                    <i className="fa fa-times" aria-hidden="true" id={ link._id } onClick={ handleOnClick }></i>
+                </span>
+                <br />
+            </li>
         ));
     } else {
+        // Links for Band Profile screen
         links = artist.bandLinks.map((link) => (
             <li key={link._id}>
                 <a href={`https://${link.siteUrl}`} target='_blank' rel='noreferrer'>
@@ -39,14 +39,14 @@ const ArtistLinks = ({ artist, isLoading, title }) => {
     // Render artist links
     return isLoading ? (<h1>Loading....</h1>) : 
     (  
-            <div className="d-flex flex-column">
-                <p style={{marginBottom: 0}}>
-                    <u>{title} Band Media Links:</u>
-                </p>
-                <ul>
-                    { links }
-                </ul>
-            </div>
+        <div className="d-flex flex-column">
+            <p style={{marginBottom: 0}}>
+                <u>{title} Band Media Links:</u>
+            </p>
+            <ul>
+                { links }
+            </ul>
+        </div>
     )
 }
 
