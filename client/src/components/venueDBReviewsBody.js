a8a0d89c0e186bbacb0f6ddb80408bc2e86973da
wylie <wyliedavid1984@gmail.com>
2021-02-22 18:39:11 -0500

    can view users and fixed nav issues

--- client/src/components/venueDBReviewsBody.js
+++ client/src/components/venueDBReviewsBody.js
@@ -1,5 +1,5 @@
-import { format } from 'morgan';
 import React, { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom'
 import Rating from 'react-rating'
 import API from "../utils/API";
 
@@ -8,6 +8,7 @@ import API from "../utils/API";
 const VenueReviewBody = ({review}) => {
 
     const [artistName, setArtistName] = useState ([])
+    const [artistId, setArtistId] = useState()
 
    const formatter = new Intl.DateTimeFormat("en-US", {
       year: "numeric",
@@ -20,6 +21,8 @@ const VenueReviewBody = ({review}) => {
           const fetchArtist = async () => {
           const result = await API.getBand(review.author)
           const artistReview = result.data
+          console.log(artistReview)
+          setArtistId(artistReview._id)
           setArtistName(artistReview.bandName)
         }
         fetchArtist()
@@ -30,7 +33,7 @@ const VenueReviewBody = ({review}) => {
       <>
         <tr>
            <td >{formatter.format(Date.parse(review.createdAt))}</td> 
-           <td>{artistName}</td>
+           <td><Link to={`/bandpage/${artistId}`}>{artistName}</Link></td>
            <td><Rating initialRating={review.rating} emptySymbol="fa fa-star-o fa-2x smallstars" readonly fullSymbol="fa fa-star fa-2x smallstars" /></td>
            <td>{review.reviewText}</td>
         </tr>
